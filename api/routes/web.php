2c8500e33ce2d2f5fd297a298669e23ba94eff1d
seojeonggwon <seo.jeonggwon.abnet@gmail.com>
2025-12-22 16:32:04 +0900

    ヘルスチェック用

--- api/routes/web.php
+++ api/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ALBヘルスチェック用
+Route::get('/healthz', function () {
+    return response()->json(['status' => 'ok'], 200);
+})->withoutMiddleware(['web']);
